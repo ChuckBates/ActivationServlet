154b6083d8e7f945ee497d85ecf5445207504398
SovietBear <trekkersforever@gmail.com>
2016-02-25 09:15:16 -0700

    Initial commit

---
+++ src/com/qbit/ActivationServlet.java
@@ -0,0 +1,38 @@
+package com.qbit;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * User: cbates
+ */
+public class ActivationServlet extends HttpServlet{
+    private static final long serialVersionID = 1L;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+
+        PrintWriter out = resp.getWriter();
+        out.println("Activation Successful");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: cbates
+  Date: 2/25/2016
+  Time: 8:38 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
+<% response.sendRedirect(getServletConfig().getServletContext().getContextPath() + "/Activate"); %>
+<%--<html>--%>
+  <%--<head>--%>
+    <%--<title>$Title$</title>--%>
+  <%--</head>--%>
+  <%--<body>--%>
+  <%--$END$--%>
+  <%--</body>--%>
+<%--</html>--%>
